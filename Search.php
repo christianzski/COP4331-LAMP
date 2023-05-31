c32cad65d51c3c5d3a11adf293fb0755e2a086a2
PrwaeenPongpat <praween.pongpat@gmail.com>
2023-05-31 15:00:00 -0400

    update Search

--- Search.php
+++ Search.php
@@ -4,18 +4,24 @@
 
 	$searchResults = "";
 	$searchCount = 0;
+    $idCheck = 0;
 
 	$conn = new mysqli("localhost", "Group21", "Group21OnTop", "COP4331");
- 
+
 	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
 	}
 	else
 	{
+	    $idCheck = validUser($conn);
+	    if($idCheck == 0){
+	        returnWithError( "User not valid within cookie" );
+	    }
+
 		$stmt = $conn->prepare("select * from Contacts where (FirstName like ? or LastName like ?) and UserID=?");
 		$searchName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("sss", $searchName, $searchName, $inData["userId"]);
+		$stmt->bind_param("ssi", $searchName, $searchName, $idCheck);
 		$stmt->execute();
 
 		$result = $stmt->get_result();
@@ -27,13 +33,13 @@
 				$searchResults .= ",";
 			}
 			$searchCount++;
-			
-			$searchResults .= '{"ID" : "' . $row["ID"] .
-				'", "FirstName" : "' . $row["FirstName"] .
-				'", "LastName" : "' . $row["LastName"] .
-				'", "Phone" : "' . $row["Phone"] .
-				'", "Email" : "' . $row["Email"] . 
-				'", "UserID" : "' . $row["UserID"].  '"}';
+
+			$searchResults .= '{"ID" : "' . $row["ID"] . '",
+				"FirstName" : "' . $row["FirstName"] . '",
+				"LastName" : "' . $row["LastName"] . '",
+				"Phone" : "' . $row["Phone"] . '",
+				"Email" : "' . $row["Email"] .	'",
+				"UserID" : "' . $row["UserID"].  '"}';
 		}
 
 		if( $searchCount == 0 )
@@ -70,6 +76,21 @@
 	{
 		$retValue = '{"results":[' . $searchResults . '],"error":""}';
 		sendResultInfoAsJson( $retValue );
+	}
+
+	function validUser($conn) //todo here ---> login = PoonP, password = testing
+	{
+	    $stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+        $stmt->bind_param("ss", $_COOKIE["login"], $_COOKIE["password"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if( $row = $result->fetch_assoc()  )
+        {
+            $idCheck = $row['ID'];
+        }
+
+        return $idCheck;
 	}
 
 ?>

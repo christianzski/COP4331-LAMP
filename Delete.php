0c60f4f473a9e47b306db636a1faad54a0963c03
PrwaeenPongpat <praween.pongpat@gmail.com>
2023-05-31 15:13:15 -0400

    Update Delete

--- Delete.php
+++ Delete.php
@@ -3,16 +3,22 @@
 	
 	$id = $inData["id"];
 	$userId = $inData["userId"];
+    $idCheck = 0;
 
 	$conn = new mysqli("localhost", "Group21", "Group21OnTop", "COP4331");
-	if ($conn->connect_error) 
+	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
-	} 
+	}
 	else
 	{
+        $idCheck = validUser($conn);
+          if($idCheck == 0){
+            returnWithError( "User not valid within cookie" );
+        }
+
 		$stmt = $conn->prepare("delete From Contacts where (UserId = ? and id = ?)");
-		$stmt->bind_param("ii", $userId, $id);
+		$stmt->bind_param("ii", $idCheck, $id);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
@@ -29,11 +35,25 @@
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
+	function validUser($conn) //todo here ---> login = PoonP, password = testing
+	{
+	    $stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+        $stmt->bind_param("ss", $_COOKIE["login"], $_COOKIE["password"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if( $row = $result->fetch_assoc()  )
+        {
+            $idCheck = $row['ID'];
+        }
+
+        return $idCheck;
+	}
 ?>
